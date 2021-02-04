862ca8e21ccf2d6bf8808d94985b3ef5b21d3567
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-04 18:32:13 -0500

    Hard coded values from the api into the table.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -15,23 +15,16 @@ const Table = () => {
         </thead>
         <tbody>
           <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <th scope="row">1NNaN50042736 92</th>
+            <td><img src="https://randomuser.me/api/portraits/thumb/men/58.jpg" alt=""/></td>
+            <td>Theo Duval</td>
+            <td>theo.duval@example.com</td>
+            <td>01-79-10-63-52</td>
+            <td>t1990-06-10T10:51:10.540Z</td>
+            
+
           </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+         
         </tbody>
       </table>
     );
